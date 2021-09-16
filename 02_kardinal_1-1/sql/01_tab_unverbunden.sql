cf3f749be53d573ef2418632200a676a923ec77f
Holger <86824184+HolgerSin@users.noreply.github.com>
2021-09-16 15:33:22 +0200

    1:1 Implementation

---
+++ 02_kardinal_1-1/sql/01_tab_unverbunden.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS `mydb`.`cats` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `cat_name` VARCHAR(45) NOT NULL,
+  `fur_color` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `mydb`.`servants` (
+  `id` INT NOT NULL,
+  `servant_name` VARCHAR(45) NOT NULL,
+  `yrs_served` INT NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
